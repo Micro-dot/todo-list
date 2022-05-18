c3513ab7c0b586be66537997621077014f34b364
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-05-19 03:47:47 +0600

    todo list updated

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-const Todo = ({todo,setTodos,todos}) => {
-    const deleteHandler = () =>{
+const Todo = ({ todo, setTodos, todos }) => {
+    const deleteHandler = () => {
         setTodos(todos.filter(el => el.id !== todo.id))
+    }
+    const completeHandle = () => {
+        setTodos(
+            todos.map((item)=>{
+                if(item.id === todo.id){
+                    return{
+                        ...item,
+                        completed: !item.completed,
+                    };
+                }
+                return item;
+            })
+        )
     }
     return (
         <div className='todo'>
-            <li className='todo-item'>{todo.Text}</li>
-            <button className='complete-btn' >
+            <li className={`todo-item ${todo.completed ? "completed":''}`}>{todo.Text}</li>
+            <button onClick={completeHandle} className='complete-btn' >
                 <i className='fas fa-check'></i>
             </button>
             <button onClick={deleteHandler} className='trash-btn'>
