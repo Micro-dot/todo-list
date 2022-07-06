f1c252cbd6aff141cd9c0e706c910f4493e9ffaf
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-07-06 23:31:39 +0600

    Date added

--- src/Components/AddTodo.js
+++ src/Components/AddTodo.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
+
 
 const AddTodo = () => {
     const [taskName, setTaskName] = useState('');
     const [taskType, setTaskType] = useState('');
     const [taskStatus, setTaskStatus] = useState('');
+    const [todos, setTodos] = useState([]);
+    useEffect(() => {
+        const data = localStorage.getItem('todos');
+        if (data) {
+            setTodos(JSON.parse(data));
+        }
+    }, [{}])
+
+    // var dateObj = new Date();
+    // var month = dateObj.getUTCMonth() + 1; //months from 1-12
+    // var day = dateObj.getUTCDate();
+    // var year = dateObj.getUTCFullYear();
+    // var newdate = day + "/" + month + "/" + year;
+    // console.log(newdate);
+
+    // 8/19/2020, 9:29:51 AM. (date and time in a specific timezone)
+    const time = (new Date().toLocaleDateString("en-US", { 
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    }));
 
     const handleTaskName = (e) => {
         setTaskName(e.target.value);
@@ -23,45 +45,26 @@ const AddTodo = () => {
         setTaskType('');
         setTaskStatus('');
 
-        // let name = [];
-        // let type = [];
-        // let status = [];
-
-        // const todo = {
-        //     name: taskName,
-        //     type: taskType,
-        //     status: taskStatus
-        // }
-        // console.log(todo);
-        // const todoList = localStorage.getItem('todos');
-        // console.log(todoList);
-        // if (!todoList) {
-        //     localStorage.setItem('todos', JSON.stringify([todo]));
-        // }
-        // else {
-        //     const gettodo = JSON.parse(localStorage.getItem('todos'));
-        //     console.log(gettodo);
-        //     gettodo.push(todo);
-        //     localStorage.setItem('todos', JSON.stringify(gettodo));
-        // }
-
         const todo = {
             name: taskName,
             type: taskType,
-            status: taskStatus
+            status: taskStatus,
+            time: time
         }
-        console.log(todo);
+        // console.log(todo);
         const todoList = localStorage.getItem('todos');
-        console.log(todoList);
+        // console.log(todoList);
         if (!todoList) {
             localStorage.setItem('todos', JSON.stringify([todo]));
         }
         else {
             const gettodo = JSON.parse(localStorage.getItem('todos'));
-            console.log(gettodo);
+            // console.log(gettodo);
             gettodo.push(todo);
             localStorage.setItem('todos', JSON.stringify(gettodo));
+            setTodos(gettodo);
         }
+
 
     }
     return (
@@ -76,6 +79,7 @@ const AddTodo = () => {
                     onChange={handleTaskName}
                     className='mt-1 pl-4 pb-1 border-t bg-slate-100 shadow-md rounded-lg lg:w-64'
                     placeholder='Name Your Task'
+                    autoComplete='off'
                     required />
                 {/* Task Type  */}
                 <h1 className='text-xl mt-4'>Task Type</h1>
@@ -96,12 +100,41 @@ const AddTodo = () => {
                     <option className="rounded">Completed</option>
                 </select><br />
                 {/* Task Submit */}
+
                 <input
                     className='mt-4 bg-teal-300 shadow-sm rounded-lg lg:w-64 w-full cursor-pointer'
                     type="submit"
                     value="Add Task" />
             </form>
+            {/* Task List */}
+            <table class="min-w-full border-collapse block md:table mt-5">
+                <thead class="block md:table-header-group">
+                    <tr class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
+                        <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Task Name</th>
+                        <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Task Type</th>
+                        <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Task Status</th>
+                        <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Date</th>
+                    </tr>
+                </thead>
+                <tbody class="block md:table-row-group">
+                    {
+                        todos.map(todo =>
+                            <tr class="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{todo.name}</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{todo.type}</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{todo.status}</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{todo.time}</td>
+                            </tr>
+                        )
+                    }
+
+                </tbody>
+            </table>
+
+
+
         </div>
+
     );
 };
 
